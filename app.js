e274a64fe420084b8c2133aae88929cee8c99785
Djones106 <djones106@gmail.com>
2021-09-15 20:11:53 -0400

    2nd updated attempt to return a random array

--- app.js
+++ app.js
@@ -8,25 +8,27 @@ const buttons = document.querySelectorAll('#qwerty');
 const phrase = document.querySelector('#phrase');
 const startGame = document.querySelector('.btn_reset');
 const overlay = document.querySelector('#overlay');
-const phraseArray = getRandomPhraseAsArray(phrases);
 
-const missed = 0;
-
-overlay.addEventListener('click', (e) => {
-    e.preventDefault();
-    overlay.style.display = 'none';
-});
+let missed = 0;
 
 const phrases = ['Smooth Criminal', 
           'Kim Possible',
           'Zero to Hero',
           'Headstrong'];
+          
+overlay.addEventListener('click', (e) => {
+    e.preventDefault();
+    overlay.style.display = 'none';
+});
+
+
 
 //console.log(phrases); testing phrases are showing
 
- function getRandomPhraseAsArray(phrases){
-    const randomPhrase = phrases[Math.floor(Math.random() * phrases.length)];
-    const split = randomPhrase.split(phrases);
+ function getRandomPhraseAsArray (phrases) {
+
+    let randomPhrase = Math.floor(Math.random() * phrases.lenght);
+    let split = randomPhrase.split('');
     return split;
     
 };
