a99373239e34d5e17978c2b732bff5addb32bd2e
Djones106 <djones106@gmail.com>
2021-09-21 17:35:25 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,40 +1,62 @@
-//prompt player choose a letter
-//set letter to guess
-//loop until all letters are quessed
-//if incorrect quesses are > 5 player loses
-//if quess is correct continue allowing quesses
+// get needed elements from html
 
-const buttons = document.querySelectorAll('#qwerty');
-const phrase = document.querySelector('#phrase');
-const startGame = document.querySelector('.btn_reset');
-const overlay = document.querySelector('#overlay');
+let buttons = document.querySelectorAll('#qwerty');
+let phrase = document.getElementById('phrase');
+let startGame = document.querySelector('.btn_reset');
+let overlay = document.getElementById('overlay');
+
+
 
 let missed = 0;
-
+// created phrases to quess
 const phrases = ['Smooth Criminal', 
           'Kim Possible',
           'Zero to Hero',
           'Headstrong'];
-          
-overlay.addEventListener('click', (e) => {
-    e.preventDefault();
-    overlay.style.display = 'none';
-});
 
-
+// created event listner to start the game and show overlay screen
+overlay.addEventListener('click', () => {
+    missed = 0;
+    overlay.style.display = "none";
+    });
 
 //console.log(phrases); testing phrases are showing
-
- function getRandomPhraseAsArray (phrases) {
-
-    let randomPhrase = Math.floor(Math.random() * phrases.lenght);
+//created function to get a random phrase and break it into individual letters
+function getRandomPhraseAsArray () {
+    let randomIndex = Math.floor(Math.random() * phrases.length);
+    let randomPhrase = phrases[randomIndex];
     let split = randomPhrase.split('');
     return split;
     
-};
- console.log(getRandomPhraseAsArray); //testing random word is chosen
-
-// getRandomPhraseAsArray(phrases);
+ }
+ const randomPhrase = getRandomPhraseAsArray();
 
 
+ //testing for random word 
+
+//console.log(getRandomPhraseAsArray())
+//created a function to dsiplay empty boxes to quess letters
+ function addPhraseToDisplay(randomPhrase){
+    for (let i = 0; i < randomPhrase.length; i++) {
+        let li = document.createElement("LI");
+        let ul = document.querySelector('ul');
+
+        li.textContent = randomPhrase[i];
+        ul.appendChild(li);
+
+        if(randomPhrase[i] != ' '){
+            li.classList.add('letter')
+            }else{
+                li.classList.add('space');
+            }
+    }
+
+ };
+ addPhraseToDisplay(randomPhrase); 
+
+//checking letters to see if they are in the random phrase 
+
+
+
+//activating keyboard on screen to reconized when clicked
 
